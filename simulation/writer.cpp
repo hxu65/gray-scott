4e6d823ab273cd97dac3e2c924252e53f53483ec
Hua Xu <97185370+hxu65@users.noreply.github.com>
2026-01-07 11:27:48 -0600

    Refactor writer.cpp by removing unused code

--- simulation/writer.cpp
+++ simulation/writer.cpp
@@ -96,19 +96,7 @@ Writer::Writer(const Settings &settings, const GrayScott &sim, adios2::IO io, bo
                                       {sim.size_z, sim.size_y, sim.size_x});
 
 
-    if(derived == 1) {
-        std::cout << "use derived variables" << std::endl;
-        auto PDFU = io.DefineDerivedVariable("derive/hashU",
-                                             "x = U \n"
-                                             "add(x)",
-                                             adios2::DerivedVarType::StoreData);
-
-        auto PDFV = io.DefineDerivedVariable("derive/hashV",
-                                             "x = V \n"
-                                             "add(x)",
-                                             adios2::DerivedVarType::StoreData);
-
-    }
+ 
 
 
 
@@ -207,4 +195,5 @@ void Writer::write(int step, const GrayScott &sim, int rank)
 }
 
 void Writer::close() { writer.Close(); }
+
 
